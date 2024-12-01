8f58b11c1f3e0ef92334042a07653a8b8c963214
r0ute <route-a@ya.ru>
2024-11-30 18:46:07 -0600

    init project

---
+++ README.md
@@ -0,0 +1,31 @@
+# RackAutoFill
+
+## BoxSize
+
+| Name | Dimensions | Cubic Volume | Description             |
+| ---- | ---------- | -----------: | ----------------------- |
+| S-C  | 8x8x8      |          512 | Small Cube              |
+| S-R  | 20x10x7    |         1400 | Small Rectangular       |
+| M-CM | 15x15x15   |         3375 | Medium Compact Cube     |
+| M-FR | 20x20x10   |         4000 | Medium Flat Rectangular |
+| M-C  | 20x20x20   |         8000 | Medium Cube             |
+| L-R  | 30x20x20   |        12000 | Large Rectangular       |
+| XL-R | 40x26x26   |        26080 | Extra Large Rectangular |
+
+## DisplayType
+
+| Abbreviation | Display Type | Color (HTML Code)                                         |
+| ------------ | ------------ | --------------------------------------------------------- |
+| F            | FREEZER      | <span style="color:#4C9AFF">Bright Sky Blue (#4C9AFF)</span> |
+| R            | FRIDGE       | <span style="color:#66D9B0">Mint Green (#66D9B0)</span>    |
+| C            | CRATE        | <span style="color:#8B4513">Saddle Brown (#8B4513)</span>  |
+| S            | SHELF        | <span style="color:#D3D3D3">Light Gray (#D3D3D3)</span>    |
+
+## ProductCategory
+
+| Abbreviation | Product Category | Color (HTML Code)                                            |
+| ------------ | ---------------- | ------------------------------------------------------------ |
+| E            | EDIBLE           | <span style="color:#A3D77A">Lime Green (#A3D77A)</span>      |
+| D            | DRINK            | <span style="color:#4682B4">Steel Blue (#4682B4)</span>      |
+| C            | CLEANING         | <span style="color:#F5F5F5">Ivory (#F5F5F5)</span>           |
+| B            | BOOK             | <span style="color:#D2B48C">Tan (#D2B48C)</span>             |
---
+++ src/Plugin.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using MyBox;
+using TMPro;
+using UnityEngine;
+using VLB;
+
+namespace SS.src;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+
+    static Color FREEZER_COLOR = new(0.298f, 0.604f, 1, 1);
+
+    static Color FRIDGE_COLOR = new(0.4f, 0.851f, 0.69f, 1);
+
+    static Color CRATE_COLOR = new(0.545f, 0.271f, 0.075f, 1);
+
+    static Color EDIBLE_COLOR = new(0.639f, 0.843f, 0.478f, 1);
+
+    static Color DRINK_COLOR = new(0.275f, 0.51f, 0.706f, 1);
+
+    static Color CLEANING_COLOR = new(0.961f, 0.961f, 0.961f, 1);
+
+    static Color BOOK_COLOR = new(0.824f, 0.706f, 0.549f, 1);
+
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+
+        Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(Patches));
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    class Patches
+    {
+
+        [HarmonyPatch(typeof(DayCycleManager), "Start")]
+        [HarmonyPostfix]
+        static void OnDayStart()
+        {
+
+            Array.ForEach((DisplayType[])Enum.GetValues(typeof(DisplayType)), new Action<DisplayType>(LogProduct));
+
+        }
+
+        private static void LogProduct(DisplayType displayType)
+        {
+            Logger.LogDebug(displayType);
+
+            Singleton<IDManager>.Instance.Products
+                .Where((product) => product.ProductDisplayType == displayType)
+                .OrderBy(product => product.Category)
+                .ThenBy(product => product.ProductName)
+                .ForEach(product => Logger.LogDebug($"Product: name={product},category={product.Category}"));
+
+        }
+
+
+        [HarmonyPatch(typeof(Label), nameof(Label.DisplaySetup))]
+        [HarmonyPostfix]
+        static void OnLabelDisplaySetup(DisplaySlot displaySlot, ref Label __instance)
+        {
+            SetColor(__instance, displaySlot.ProductID);
+        }
+
+
+        [HarmonyPatch(typeof(Label), nameof(Label.RackSetup))]
+        [HarmonyPostfix]
+        static void OnLabelRackSetup(RackSlot rackSlot, ref Label __instance)
+        {
+            SetColor(__instance, rackSlot.Data.ProductID);
+        }
+
+        private static void SetColor(Label label, int productId)
+        {
+            var product = Singleton<IDManager>.Instance.ProductSO(productId);
+            var color = Color.white;
+
+            if (product.ProductDisplayType == DisplayType.FRIDGE)
+            {
+                color = FRIDGE_COLOR;
+            }
+            else if (product.ProductDisplayType == DisplayType.FREEZER)
+            {
+                color = FREEZER_COLOR;
+            }
+            else if (product.ProductDisplayType == DisplayType.CRATE)
+            {
+                color = CRATE_COLOR;
+            }
+            else if (product.Category == ProductSO.ProductCategory.EDIBLE)
+            {
+                color = EDIBLE_COLOR;
+            }
+            else if (product.Category == ProductSO.ProductCategory.DRINK)
+            {
+                color = DRINK_COLOR;
+            }
+            else if (product.Category == ProductSO.ProductCategory.CLEANING)
+            {
+                color = CLEANING_COLOR;
+            }
+            else if (product.Category == ProductSO.ProductCategory.BOOK)
+            {
+                color = BOOK_COLOR;
+            }
+
+
+            label.transform.Find("UI/Icon BG").GetComponent<MeshRenderer>().material.color = color;
+        }
+
+
+    }
+}
